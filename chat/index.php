4af58f12eeb0a575a44ed611c0d732afd3b6db5d
portable-games <54112835+portable-games@users.noreply.github.com>
2019-08-14 19:03:41 +0300

    Delete index.php

--- chat/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Chat | portable-games.ga</title>
-		<link rel="icon" type="image/x-icon" href="../website_icon.ico"/>
-	</head>
-	<body onload="window.location.href='https://discord.gg/VS3HtJg'">
-	</body>
-</html>
