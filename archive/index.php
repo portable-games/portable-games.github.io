5d07cb66963fed3390d372ea6a638f90cbca0ef5
portable-games <eric.jarnea12345361@protonmail.com>
2019-08-14 18:34:23 +0300

    Initial commit

---
+++ archive/index.php
@@ -0,0 +1,72 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Archive | portable-games.ga</title>
+		<link rel="icon" type="image/x-icon" href="../website_icon.ico"/>
+		<style media="screen">      
+			@import url('https://fonts.googleapis.com/css?family=Lobster');
+		
+			html {
+          		background: url("https://i.imgur.com/Nks1nfZ.jpg") no-repeat center center fixed;
+          		background-size: cover;
+          		height: 100%;
+          		overflow: hidden;
+            }
+
+			.topnav {
+			  font-family: Lobster;
+			  background-color: #333;
+			  overflow: hidden;
+			  position: sticky;
+			  position: -webkit-sticky;
+			  top: 0;
+			  width: 100%;
+			}
+
+			.topnav a {
+			  float: left;
+			  color: #f2f2f2;
+			  text-align: center;
+			  padding: 14px 16px;
+			  text-decoration: none;
+			  font-size: 17px;
+			}
+			
+			#a1:hover {
+				background-color: #ddd;
+				color: black;
+			}
+			
+			#a2:hover {
+				background-color: #ddd;
+				color: black;
+			}
+			
+			#a3:hover {
+				background-color: #ddd;
+				color: black;
+			}
+			
+			#a4:hover {
+				background-color: #ddd;
+				color: black;
+			}
+			
+			.topnav a.active {
+				background-color: #4CAF50;
+				color: white;
+			}
+		</style>
+	</head>
+	<body style="text-align: center;">
+		<div class="topnav">
+			<a href="index.php">
+				<img src="../website_logo.png" style="width: 200px;" />
+			</a>
+			<a id="a1" style="float: right;" href="../about/index.php">About</a>
+			<a class="active" id="a2" style="float: right;" href="index.php">Archive</a>
+			<a id="a3" style="float: right;" href="../chat/index.php">Chat</a>
+			<a id="a4" style="float: right;" href="../support/index.php">Support</a>
+		</div>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>portable-games.ga | Games & Software</title>
+		<link rel="icon" type="image/x-icon" href="website_icon.ico"/>
+		<style media="screen">      
+			@import url('https://fonts.googleapis.com/css?family=Lobster');
+		
+			html {
+          		background: url("https://i.imgur.com/Nks1nfZ.jpg") no-repeat center center fixed;
+          		background-size: cover;
+          		height: 100%;
+          		overflow: hidden;
+            }
+
+			.topnav {
+			  font-family: Lobster;
+			  background-color: #333;
+			  overflow: hidden;
+			  position: sticky;
+			  position: -webkit-sticky;
+			  top: 0;
+			  width: 100%;
+			}
+
+			.topnav a {
+			  float: left;
+			  color: #f2f2f2;
+			  text-align: center;
+			  padding: 14px 16px;
+			  text-decoration: none;
+			  font-size: 17px;
+			}
+			
+			#a1:hover {
+				background-color: #ddd;
+				color: black;
+			}
+			
+			#a2:hover {
+				background-color: #ddd;
+				color: black;
+			}
+			
+			#a3:hover {
+				background-color: #ddd;
+				color: black;
+			}
+			
+			#a4:hover {
+				background-color: #ddd;
+				color: black;
+			}
+		</style>
+	</head>
+	<body style="text-align: center;">
+		<div class="topnav">
+			<a href="index.php">
+				<img src="website_logo.png" style="width: 200px;" />
+			</a>
+			<a id="a1" style="float: right;" href="about/index.php">About</a>
+			<a id="a2" style="float: right;" href="archive/index.php">Archive</a>
+			<a id="a3" style="float: right;" href="chat/index.php">Chat</a>
+			<a id="a4" style="float: right;" href="support/index.php">Support</a>
+		</div>
+	</body>
+</html>
